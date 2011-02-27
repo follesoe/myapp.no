d37761901ba3be1935404e78200aeb0c8ed374ee
Jonas Folleso <jonas@follesoe.no>
2011-02-26 19:00:13 -0800

    Added privacy policy.

--- app.rb
+++ app.rb
@@ -24,6 +24,10 @@ get "/BigProfileAppStore" do
   redirect "http://itunes.apple.com/no/app/facebook-big-profile/id417118743?mt=8"
 end
 
+get "/BigProfilePrivacy" do
+  erb :bigprofileprivacy
+end
+
 get %r{^/bigprofile/?$}i do
   @title = "Facebook Big Profile"
   erb :bigprofile
